cc2b9c3a8baebdafa74f3fc458c6cc695414e514
Lyn265 <chenglliu2018@gmail.com>
2021-04-18 23:46:31 +0900

    init

---
+++ src/test/java/com/lyn/wiki/WikiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lyn.wiki;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WikiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
